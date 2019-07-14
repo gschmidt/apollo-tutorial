8f3f18ee0c66f2ecf5cc289997a8861e417fcd55
Geoff Schmidt <geoff@geoffschmidt.com>
2019-07-14 14:50:17 -0700

    get mutations working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
+const isEmail = require('isemail');
 
 const LaunchAPI = require('./datasources/launch')
 const UserAPI = require('./datasources/user')
@@ -17,7 +18,15 @@ const server = new ApolloServer({
   }),
   engine: {
     apiKey: "service:geofftest:iNN-ouAFRHy_ifCg6zOfuQ"
-    }
+  },
+  context: async ({ req }) => {
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (! isEmail.validate(email)) return { user: null };
+    const users = await store.users.findOrCreate({ where: { email }});
+    const user = users && users[0] ? users[0] : null;
+    return { user: { ...user.dataValues } };
+  }
 });
 
 server.listen().then(({ url }) => {
